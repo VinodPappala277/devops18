7e3fa170570d897773e9f49d3ded45edf15e78dc
VinodPappala277 <vinodpappala37@gmail.com>
2026-03-10 12:34:31 +0530

    Update AMI and key name in main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-02dfbd4ff395f2a1b"
+    image_id           = "ami-0b0b78dcacbab728f"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "march-kp"
+    key_name = "mykey"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-05b5c04b3e4f5fc97", "subnet-078eaa4691fce41d5"]
+     subnets = ["subnet-0302be7c0dc0a9a25", "subnet-0e302273d479ebdde"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
